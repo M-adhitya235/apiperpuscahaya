bcd1bbfd30ab206ad59c21451652dda23fdb5ffb
M-adhitya235 <11211053@student.itk.ac.id>
2024-08-07 20:57:47 +0800

    deploit

--- routes/MemberRoute.js
+++ routes/MemberRoute.js
@@ -9,8 +9,8 @@ import { verifyToken } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/members', verifyToken, getMembers); 
-router.get('/members/:id', verifyToken, getMemberById); 
+router.get('/members', getMembers); 
+router.get('/members/:id', getMemberById); 
 router.patch('/members/:id', verifyToken, updateMember);
 router.delete('/members/:id', verifyToken, deleteMember);
 
