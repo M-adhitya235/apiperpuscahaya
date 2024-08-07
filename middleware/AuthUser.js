f50a6f8b1a0b8193b9fa65203332a80cda34f37f
M-adhitya235 <11211053@student.itk.ac.id>
2024-08-07 23:34:35 +0800

    deploit

--- middleware/AuthUser.js
+++ middleware/AuthUser.js
@@ -3,20 +3,14 @@ import User from '../models/UserModel.js';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-export const verifyToken = (req, res, next) => {
+export const verifyToken = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];  // Mengambil token dari header
 
-    if (!token) {
-        console.log("Token tidak ditemukan");
-        return res.status(401).json({ msg: "Token tidak ditemukan" });
-    }
+    if (!token) return res.status(401).json({ msg: "Token tidak ditemukan" });
 
     jwt.verify(token, JWT_SECRET, async (err, user) => {
-        if (err) {
-            console.log("Token tidak valid");
-            return res.status(403).json({ msg: "Token tidak valid" });
-        }
+        if (err) return res.status(403).json({ msg: "Token tidak valid" });
 
         // Menyimpan informasi pengguna dalam objek permintaan
         req.user = user;
@@ -24,17 +18,11 @@ export const verifyToken = (req, res, next) => {
         // Verifikasi user di database jika diperlukan
         try {
             const dbUser = await User.findOne({ where: { uuid: user.uuid } });
-            if (!dbUser) {
-                console.log("User tidak ditemukan di database");
-                return res.status(404).json({ msg: "User tidak ditemukan" });
-            }
+            if (!dbUser) return res.status(404).json({ msg: "User tidak ditemukan" });
 
-            // Log untuk memastikan dbUser.id ada
-            console.log("dbUser:", dbUser);
-
-            // Menambahkan userId ke objek permintaan
+            // Menambahkan userId dan role ke objek permintaan
             req.userId = dbUser.id;
-            console.log("User ditemukan:", dbUser.id);
+            req.role = dbUser.role;
             next();
         } catch (error) {
             console.log("Error saat mencari user di database:", error);
@@ -47,10 +35,7 @@ export const verifyToken = (req, res, next) => {
 export const adminOnly = (req, res, next) => {
     const { role } = req.user;  // Mengambil role dari token
 
-    if (role !== "admin") {
-        console.log("Akses terlarang untuk role:", role);
-        return res.status(403).json({ msg: "Akses terlarang" });
-    }
+    if (role !== "admin") return res.status(403).json({ msg: "Akses terlarang" });
 
     next();
 };
